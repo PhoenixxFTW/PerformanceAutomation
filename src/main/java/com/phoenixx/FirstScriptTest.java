a02921ff320c7b1efc9a808a654b487e46eb4192
Junaid Talpur <Junaid.Talpur@ontario.ca>
2023-07-07 10:47:12 -0400

    Found a better way to separate tags and values

--- src/main/java/com/phoenixx/FirstScriptTest.java
+++ src/main/java/com/phoenixx/FirstScriptTest.java
@@ -15,11 +15,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FirstScriptTest {
 
@@ -27,6 +27,11 @@ public class FirstScriptTest {
             "Click Hydra",
             "abc"
     ));
+
+    // HashMap to store tags and their corresponding data
+    private final static Map<String, List<String>> tagDataMap = new HashMap<>();
+
+    private static int currentIndex = 0;
 
     public static void main(String[] args) {
 
@@ -53,10 +58,58 @@ public class FirstScriptTest {
         String pageSource = driver.getPageSource();
         String allVisualStrings = Jsoup.parse(pageSource).text();
 
-        Document document = Jsoup.parse(driver.getPageSource());
-        Element element = document.selectFirst(":containsOwn("+clickOnElement+")");
+        // Convert the page source into our tag map
+        convertToMap(pageSource, false);
 
-        System.out.println("Element: " + element);
+        // The button the client should find and click on
+        String clickedOnTag = getTagFromName(clickOnElement);
+
+        // The element found, and clicked
+        WebElement element = driver.findElement(By.xpath("//" + clickedOnTag + "[text()=\"" + clickOnElement + "\"]"));
+        element.click();
+
+        // New page source after clicking
+        pageSource = driver.getPageSource();
+
+        // Convert new page source to map
+        convertToMap(pageSource, true);
+
+        // Click on "Issues" element
+        clickOnElement = "Pull Requests";
+
+        boolean success = false;
+
+        while (!success && currentIndex < tagDataMap.values().size()) {
+            try {
+                clickedOnTag = getTagFromName(clickOnElement);
+
+                // The element found, and clicked
+                //element = driver.findElement(By.xpath("//" + clickedOnTag + "[text()=\"" + clickOnElement + "\"]"));
+                element = new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.elementToBeClickable(By.xpath("//" + clickedOnTag + "[text()=\"" + clickOnElement + "\"]")));
+
+                System.out.println("ELEMENT: " + element.getAccessibleName() + " , " + element.getTagName() + ", " + element.getText());
+
+                element.click();
+            } catch (Exception e) {
+
+                System.out.println("Failed to click element: '" + clickOnElement + "' with tag: '" + clickedOnTag + "' INDEX: " + currentIndex);
+
+                // If the click failed, means we're on the wrong element, so we increment the counter and do the request again
+                currentIndex++;
+            } finally {
+
+            }
+        }
+
+        if(success) {
+            System.out.println("FOUND ELEMENT AT INDEX: " + currentIndex);
+            currentIndex = 0;
+        } else {
+            System.out.println("FAILURE @@@@@@@@@@@@@@@@@@@@@@@@");
+            return;
+        }
+
+     /*   System.out.println("Element: " + element);
         System.out.println("JSOUP: " + (jsoupToXpath(element)));
         System.out.println("JSOUP 2: " + (getXPath(element)));
 
@@ -80,60 +133,49 @@ public class FirstScriptTest {
         //driver.findElement(By.xpath("//span[@title=\"Hydra\"]")
         String title = driver.getTitle();
         //System.out.println("Title: " + title);
-
-        driver.quit();
+*/
+        //driver.quit();
     }
 
-    public static String getXPath(Element el) {
-        int index = el.elementSiblingIndex();
-        String tagName = el.tagName();
-        String xPath = "/" + tagName;
-
-        for (Element parent = el.parent(); parent != null; parent = parent.parent()) {
-            xPath = "/" + parent.tagName() + "[" + (parent.elementSiblingIndex() + 1) + "]" + xPath;
-        }
-
-        return xPath;
-    }
-
-
-    public static String jsoupToXpath(Element element) {
-        String xpath = "/";
-        List<String> components = new ArrayList<>();
-
-        Element child = element.tagName().isEmpty() ? element.parent() : element;
-        System.out.println(child.tag());
-        while (child.parent() != null){
-            Element parent = child.parent();
-            Elements siblings = parent.children();
-            String componentToAdd = null;
-
-            if (siblings.size() == 1) {
-                componentToAdd = child.tagName();
-            } else {
-                int x = 1;
-                for(Element sibling: siblings){
-                    if (child.tagName().equals(sibling.tagName())){
-                        if (child == sibling){
-                            break;
-                        } else {
-                            x++;
-                        }
+    public static String getTagFromName(String name) {
+        int counter = 0;
+        for(String tag: tagDataMap.keySet()) {
+            if(counter >= currentIndex) {
+                for (String val : tagDataMap.get(tag)) {
+                    if (val.equalsIgnoreCase(name)) {
+                        return tag;
                     }
                 }
-                componentToAdd = String.format("%s[%d]", child.tagName(), x);
             }
-            components.add(componentToAdd);
-            child = parent;
+            counter++;
         }
-
-        List<String> reversedComponents = new ArrayList<>();
-        for (int i = components.size()-1; i > 0; i--){
-            reversedComponents.add(components.get(i));
-        }
-        xpath = xpath + String.join("/", reversedComponents);
-
-        return xpath;
+        return "";
     }
 
+    public static void convertToMap(String pageSource, boolean print) {
+        tagDataMap.clear();
+
+        Document document = Jsoup.parse(pageSource);
+
+        // Parse the HTML
+        Elements allElements = document.getAllElements();
+
+        for (Element element : allElements) {
+            String tagName = element.tagName();
+
+            if (!element.ownText().isEmpty()) {
+                List<String> dataList = tagDataMap.getOrDefault(tagName, new ArrayList<>());
+                dataList.add(element.ownText());
+                tagDataMap.put(tagName, dataList);
+            }
+        }
+
+        if(print) {
+            for (Map.Entry<String, List<String>> entry : tagDataMap.entrySet()) {
+                System.out.println("Tag: " + entry.getKey());
+                System.out.println("Data: " + entry.getValue());
+                System.out.println("-------------------------------------------------");
+            }
+        }
+    }
 }
--- src/main/java/com/phoenixx/Main.java
+++ src/main/java/com/phoenixx/Main.java
@@ -1,5 +1,15 @@
 package com.phoenixx;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Junaid Talpur
  * @project ${PROJECT_NAME}
@@ -7,6 +17,29 @@ package com.phoenixx;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String html = "<html><head><title>My title</title></head><body><p>Paragraph 1</p><p>Paragraph 2</p></body></html>";
+
+        // HashMap to store tags and their corresponding data
+        Map<String, List<String>> tagDataMap = new HashMap<>();
+
+        // Parse the HTML
+        Document doc = Jsoup.parse(html);
+        Elements allElements = doc.getAllElements();
+
+        for (Element element : allElements) {
+            String tagName = element.tagName();
+
+            if (!element.ownText().isEmpty()) {
+                List<String> dataList = tagDataMap.getOrDefault(tagName, new ArrayList<>());
+                dataList.add(element.ownText());
+                tagDataMap.put(tagName, dataList);
+            }
+        }
+
+        for (Map.Entry<String, List<String>> entry : tagDataMap.entrySet()) {
+            System.out.println("Tag: " + entry.getKey());
+            System.out.println("Data: " + entry.getValue());
+            System.out.println("-------------------------------------------------");
+        }
     }
 }
